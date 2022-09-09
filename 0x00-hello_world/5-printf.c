f222e58640136dbc6d5fb2da09edeb3c17d333d8
JermaineMofe <holadekingjnr@gmail.com>
2022-09-09 02:59:08 -0700

    c_lang.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - printing
  *
- * return: always 0 (successful)
+ * Return: always 0 (successful)
  *
  */
 
@@ -11,5 +11,6 @@ int main(void)
 
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
+
 	return (0);
 }
