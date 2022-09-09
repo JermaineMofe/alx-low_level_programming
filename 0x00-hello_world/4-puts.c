45226f3281ee747d4df3c30ea783a3c79ed06288
JermaineMofe <holadekingjnr@gmail.com>
2022-09-08 21:56:01 -0700

    C lang.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multil		   ingual puzzle");
 	return (0);
 }
