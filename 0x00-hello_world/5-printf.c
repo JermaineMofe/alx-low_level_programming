9883d5508b1aa241866ae118f242778f8ae1e0bf
JermaineMofe <holadekingjnr@gmail.com>
2022-09-10 08:16:55 -0700

    num_5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-/**
- * main-printing a message
- *
- * return: 0 always (successful)
- *
- */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-
-/**
- * main - printing sizes of varying variables
- *
- * return: 0 (successful)
- *
- */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a char:%d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
