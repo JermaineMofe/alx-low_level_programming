b9954f0fa6de802e968c9516a67c54c3f002cfca
JermaineMofe <holadekingjnr@gmail.com>
2022-09-12 10:59:30 -0700

    task 8n6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print quote sizes
- *
- * return 0 (successful)
- *
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	int charSize = sizeof(char);
-	int intSize = sizeof(int);
-	int longSize = sizeof(long int);
-	int longlongSize = sizeof(long long int);
-	int floatSize = sizeof(float);
-
-	printf("size of a char: %d byte(s)\n", charSize);
-	printf("size of an int: %d byte(s)\n", intSize);
-	printf("size of a long int: %d byte(s)\n", longSize);
-	printf("size of a long long int: %d byte(s)\n", longlongSize);
-	printf("size of a float: %d byte(s)\n", floatSize);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
