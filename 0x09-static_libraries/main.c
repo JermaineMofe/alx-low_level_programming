704eeb13e66705f90ef3bb8f8fcaea81c2c3a535
JermaineMofe <holadekingjnr@gmail.com>
2022-10-01 03:30:57 -0700

    created my first static lib.

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+/**
+* creating a static library
+*/
+
+#include "main.h"
+
+int main(void)
+{
+	_puts("Creating my first static lib\n");
+	return (0);
+}
---
+++ 0x09-static_libraries/static_lib.c
@@ -0,0 +1,12 @@
+/**
+*creating my first static lib.
+*/
+
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("creating my first static lib\n");
+	return (0);
+}
