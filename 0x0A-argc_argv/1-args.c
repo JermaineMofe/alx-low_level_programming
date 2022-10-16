a577edfd4ff7bc73fc9bd1897b65951f10474ab9
JermaineMofe <holadekingjnr@gmail.com>
2022-10-16 15:22:04 -0700

    my first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguemnts
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print all arguments including file name
+ * Return: zero
+ **/
+int main(int argc, char *argv[])
+
+{
+printf("%d\n", argc - 1);
+argv = argv;
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+learning and praticing argv and argc
